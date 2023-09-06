e37543499d214fe85bc44a89f05636063208a8a3
Yana <44026935+yanajulias@users.noreply.github.com>
2023-09-06 09:15:11 +0700

    Create factory

---
+++ app/src/main/java/com/hightech/cryptoapp/frameworks/DbFactory.kt
@@ -0,0 +1,41 @@
+package com.hightech.cryptoapp.frameworks
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.hightech.cryptoapp.crypto.feed.db.CryptoFeedDao
+import com.hightech.cryptoapp.crypto.feed.db.LocalCryptoFeedItem
+
+class DbFactory {
+    @Database(
+        entities = [LocalCryptoFeedItem::class],
+        version = 1,
+        exportSchema = false
+    )
+    abstract class AppDatabase : RoomDatabase() {
+        abstract fun cryptoFeedDao(): CryptoFeedDao
+    }
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun createDatabase(context: Context): AppDatabase {
+            synchronized(this) {
+                var instance = INSTANCE
+
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        AppDatabase::class.java,
+                        "database_app"
+                    ).build()
+
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
+}
