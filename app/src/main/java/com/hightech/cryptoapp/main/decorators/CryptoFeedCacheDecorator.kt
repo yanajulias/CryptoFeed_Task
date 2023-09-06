cc21f5d484337ca3bcbb1fb4eaf50c37a5ecf1ab
Yana <44026935+yanajulias@users.noreply.github.com>
2023-09-06 09:16:34 +0700

    Create cache decorator

---
+++ app/src/main/java/com/hightech/cryptoapp/main/decorators/CryptoFeedCacheDecorator.kt
@@ -0,0 +1,21 @@
+package com.hightech.cryptoapp.main.decorators
+
+import com.hightech.cryptoapp.crypto.feed.domain.usecases.CryptoFeedLoader
+import com.hightech.cryptoapp.crypto.feed.domain.usecases.CryptoFeedResult
+import com.hightech.cryptoapp.crypto.feed.domain.usecases.InsertCryptoFeed
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class CryptoFeedCacheDecorator (
+    private val cryptoFeedLoader: CryptoFeedLoader,
+    private val cache : InsertCryptoFeed
+): CryptoFeedLoader {
+    override fun load(): Flow<CryptoFeedResult> = flow {
+        cryptoFeedLoader.load().collect{ result ->
+            if (result is CryptoFeedResult.Success){
+                cache.insert(result.cryptoFeedItems)
+            }
+            emit(result)
+        }
+    }
+}
