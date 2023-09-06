2b1e03ddbd80a9485e331ff94c6904b06ee3d391
Yana <44026935+yanajulias@users.noreply.github.com>
2023-09-06 09:15:47 +0700

    Create fallback

---
+++ app/src/main/java/com/hightech/cryptoapp/main/composite/CryptoFeedLoaderWithFallback.kt
@@ -0,0 +1,23 @@
+package com.hightech.cryptoapp.main.composite
+
+import com.hightech.cryptoapp.crypto.feed.domain.usecases.CryptoFeedLoader
+import com.hightech.cryptoapp.crypto.feed.domain.usecases.CryptoFeedResult
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class CryptoFeedLoaderWithFallback(
+    private val primary: CryptoFeedLoader,
+    private val fallback: CryptoFeedLoader
+) : CryptoFeedLoader {
+    override fun load(): Flow<CryptoFeedResult> = flow {
+        primary.load().collect { cryptoFeedResult ->
+            when (cryptoFeedResult) {
+                is CryptoFeedResult.Success -> emit(cryptoFeedResult)
+                is CryptoFeedResult.Failure -> {
+                    fallback.load().collect { emit(it) }
+                }
+            }
+
+        }
+    }
+}
